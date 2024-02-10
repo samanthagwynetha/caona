8a2351036d63bd86b4e36346480df250627a30a0
Samantha Arsua <samanthagwynetha@gmail.com>
2024-02-10 16:13:34 +0800

    2/10/2024

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+?>
+
+<section class="home" id="home">
+    <div class="home-text">
+        <h1>
+         Welcome to Our Recipe Portal!</h1>
+        <p>Explore Delicious Recipes Shared by Our Community </p>
+    </div>
+</section>
+
+<section></section>
